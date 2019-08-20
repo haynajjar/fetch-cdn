be8e0ddac748bf32a0334faec98fd7727e5ed493
haynajjar <najjar.abdelhay@gmail.com>
2019-08-20 11:32:53 +0100

    add module

---
+++ pkg/dist-web/index.js
@@ -0,0 +1,24 @@
+const options = {
+  method: 'GET',
+  headers: new Headers(),
+  mode: 'cors',
+  cache: 'default'
+};
+function fetchCDN(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url, options).then(response => {
+      if (response.ok) {
+        response.blob().then(blob => {
+          resolve(URL.createObjectURL(blob));
+        });
+      } else {
+        reject(`Response error, status: ${response.status}`);
+      }
+    }).catch(error => {
+      reject(`Problem while fetching ${url} , ${error.message}`);
+    });
+  });
+}
+
+export default fetchCDN;
+//# sourceMappingURL=index.js.map
